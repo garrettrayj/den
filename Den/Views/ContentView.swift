60c7434ceed6a25257fe1d1bb94fb33401edc3a8
Garrett Johnson <garrettrayj@gmail.com>
2021-01-04 01:30:35 -0700

    Make crash manager popup a sheet

--- Den/Views/ContentView.swift
+++ Den/Views/ContentView.swift
@@ -21,35 +21,37 @@ struct ContentView: View {
         NavigationView {
             // Sidebar
             SidebarView(pages: pages)
+                .sheet(isPresented: $subscriptionManager.showSubscribe) {
+                    if self.pages.count > 0 {
+                        SubscribeView(pages: self.pages)
+                            .environment(\.managedObjectContext, self.viewContext)
+                            .environmentObject(self.subscriptionManager)
+                            .environmentObject(self.refreshManager)
+                    } else {
+                        VStack(spacing: 16) {
+                            Text("Page Required").font(.title)
+                            Text("Create a new page before subscribing to feeds.")
+                            Button(action: { self.subscriptionManager.reset() }) {
+                                Text("Close").fontWeight(.medium)
+                            }.buttonStyle(BorderedButtonStyle())
+                        }
+                    }
+                }
             
             // Default view for detail area
             WelcomeView(pages: pages)
         }
-        .sheet(isPresented: $subscriptionManager.showSubscribe) {
-            if self.pages.count > 0 {
-                SubscribeView(pages: self.pages)
-                    .environment(\.managedObjectContext, self.viewContext)
-                    .environmentObject(self.subscriptionManager)
-                    .environmentObject(self.refreshManager)
-            } else {
-                VStack(spacing: 16) {
-                    Text("Page Required").font(.title)
-                    Text("Create a new page before subscribing to feeds.")
-                    Button(action: { self.subscriptionManager.reset() }) {
-                        Text("Close").fontWeight(.medium)
-                    }.buttonStyle(BorderedButtonStyle())
+        .sheet(isPresented: $crashManager.showingAlert) {
+            VStack() {
+                Image(systemName: "ladybug").resizable().scaledToFit().frame(width: 48, height: 48).padding()
+                Text("Application Crashed").font(.title)
+                VStack {
+                    Text("A critical error occurred. Quit the app and restart to try again. Please consider sending a bug report if you see this repeatedly.")
                 }
-            }
-        }
-        .alert(isPresented: $crashManager.showingAlert) {
-            Alert(
-                title: Text("Application Crashed"),
-                message: Text("A critical error occurred. It's not your fault. Please restart and consider sending a bug report if you see this repeatedly."),
-                dismissButton: Alert.Button.default(
-                    Text("Close"),
-                    action: { exit(1) }
-                )
-            )
+                .foregroundColor(Color(.secondaryLabel))
+                .padding()
+                .multilineTextAlignment(.center)
+            }.padding()
         }
     }
 }
