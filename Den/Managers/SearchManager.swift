99fb31c6121b601a09ae70ab8017b29704eb44d6
Garrett Johnson <garrettrayj@gmail.com>
2021-08-02 11:37:02 -0600

    Fix search CPU usage

--- Den/Managers/SearchManager.swift
+++ Den/Managers/SearchManager.swift
@@ -6,26 +6,100 @@
 //  Copyright © 2021 Garrett Johnson. All rights reserved.
 //
 
-import Foundation
+import CoreData
 
 final class SearchManager: ObservableObject {
     @Published var searchQuery: String = ""
     @Published var searchResults: [[Item]] = []
     @Published var historyResults: [[History]] = []
 
+    private var viewContext: NSManagedObjectContext
+    private var crashManager: CrashManager
+
+    init(viewContext: NSManagedObjectContext, crashManager: CrashManager) {
+        self.viewContext = viewContext
+        self.crashManager = crashManager
+    }
+
     func reset() {
         searchQuery = ""
     }
 
-    func trimQuery() {
-        searchQuery = searchQuery.trimmingCharacters(in: .whitespacesAndNewlines)
-    }
-
     func searchIsValid() -> Bool {
-        if searchQuery == "" || searchQuery.count >= 3 {
+        if searchQuery.count >= 3 {
             return true
         }
 
         return false
+    }
+
+    func performItemSearch() {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
+        fetchRequest.predicate = NSPredicate(
+            format: "%K CONTAINS[C] %@",
+            #keyPath(Item.title),
+            searchQuery
+        )
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Item.published, ascending: false)]
+        fetchRequest.relationshipKeyPathsForPrefetching = ["feed"]
+
+        do {
+            guard let fetchResults = try viewContext.fetch(fetchRequest) as? [Item] else { return }
+            var compactedFetchResults: [Item] = []
+            fetchResults.forEach { item in
+                if item.feedData?.feed != nil {
+                    compactedFetchResults.append(item)
+                }
+            }
+
+            searchResults = Dictionary(grouping: compactedFetchResults) { item in
+                item.feedData!
+            }.values.sorted { aItem, bItem in
+                guard
+                    let aTitle = aItem[0].feedData?.feed?.wrappedTitle,
+                    let bTitle = bItem[0].feedData?.feed?.wrappedTitle
+                else {
+                    return false
+                }
+
+                return aTitle < bTitle
+            }
+        } catch {
+            crashManager.handleCriticalError(error as NSError)
+        }
+    }
+
+    func performHistorySearch() {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "History")
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \History.visited, ascending: false)]
+
+        if searchQuery != "" {
+            fetchRequest.predicate = NSPredicate(
+                format: "%K CONTAINS[C] %@",
+                #keyPath(History.title),
+                searchQuery
+            )
+        }
+
+        do {
+            guard let fetchResults = try viewContext.fetch(fetchRequest) as? [History] else { return }
+            var compactedFetchResults: [History] = []
+            fetchResults.forEach { history in
+                if history.visited != nil {
+                    compactedFetchResults.append(history)
+                }
+            }
+
+            let grouping = Dictionary(
+                grouping: compactedFetchResults,
+                by: { DateFormatter.isoDate.string(from: $0.visited!) }
+            )
+
+            historyResults = grouping.values.sorted { aHistory, bHistory in
+                return aHistory[0].visited! > bHistory[0].visited!
+            }
+        } catch {
+            crashManager.handleCriticalError(error as NSError)
+        }
     }
 }
--- Den/Views/SearchView.swift
+++ Den/Views/SearchView.swift
@@ -42,59 +42,8 @@ struct SearchView: View {
             .background(Color(UIColor.secondarySystemBackground).edgesIgnoringSafeArea(.all))
             .navigationTitle("Search")
             .navigationBarTitleDisplayMode(.inline)
-            .onReceive(
-                searchManager.searchQuery
-                    .publisher
-                    .removeDuplicates()
-                    .debounce(for: .seconds(0.5), scheduler: RunLoop.main)
-                    .collect(),
-                perform: { _ in
-                    DispatchQueue.main.async {
-                        searchManager.trimQuery()
-                        if searchManager.searchIsValid() {
-                            self.search(query: searchManager.searchQuery)
-                        }
-                    }
 
-                }
-            )
         }
         .navigationViewStyle(StackNavigationViewStyle())
-    }
-
-    private func search(query: String) {
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Item")
-        fetchRequest.predicate = NSPredicate(
-            format: "%K CONTAINS[C] %@",
-            #keyPath(Item.title),
-            query
-        )
-        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Item.published, ascending: false)]
-        fetchRequest.relationshipKeyPathsForPrefetching = ["feed"]
-
-        do {
-            guard let fetchResults = try viewContext.fetch(fetchRequest) as? [Item] else { return }
-            var compactedFetchResults: [Item] = []
-            fetchResults.forEach { item in
-                if item.feedData?.feed != nil {
-                    compactedFetchResults.append(item)
-                }
-            }
-
-            searchManager.searchResults = Dictionary(grouping: compactedFetchResults) { item in
-                item.feedData!
-            }.values.sorted { aItem, bItem in
-                guard
-                    let aTitle = aItem[0].feedData?.feed?.wrappedTitle,
-                    let bTitle = bItem[0].feedData?.feed?.wrappedTitle
-                else {
-                    return false
-                }
-
-                return aTitle < bTitle
-            }
-        } catch {
-            crashManager.handleCriticalError(error as NSError)
-        }
     }
 }
