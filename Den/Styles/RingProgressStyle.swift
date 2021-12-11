d557df1b5479b229ac2d04cc39d6b4b1e831bd35
Garrett Johnson <garrettrayj@gmail.com>
2021-12-11 05:44:11 -0700

    Better ring progress style.

--- Den/Styles/RingProgressStyle.swift
+++ Den/Styles/RingProgressStyle.swift
@@ -18,19 +18,21 @@ struct RingProgressStyle: ProgressViewStyle {
             if triggered {
                 ProgressView().progressViewStyle(.circular).scaleEffect(1.5)
             } else {
-                Circle()
-                    .stroke(lineWidth: lineWidth)
-                    .foregroundColor(Color(UIColor.secondarySystemFill))
+                Group {
+                    Circle()
+                        .stroke(lineWidth: lineWidth)
+                        .foregroundColor(Color(UIColor.secondarySystemFill))
 
-                Circle()
-                    .trim(from: 0.0, to: CGFloat(min(configuration.fractionCompleted ?? 0, 1.0)))
-                    .stroke(style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
-                    .foregroundColor(Color(UIColor.systemFill))
-                    .rotationEffect(Angle(degrees: 270.0))
-                    .animation(.linear, value: configuration.fractionCompleted)
+                    Circle()
+                        .trim(from: 0.0, to: CGFloat(min(configuration.fractionCompleted ?? 0, 1.0)))
+                        .stroke(style: StrokeStyle(lineWidth: lineWidth, lineCap: .round, lineJoin: .round))
+                        .foregroundColor(Color(UIColor.systemFill))
+                        .rotationEffect(Angle(degrees: 270.0))
+                        .animation(.linear, value: configuration.fractionCompleted)
+                }
+                .opacity(min(configuration.fractionCompleted ?? 0, 1))
             }
         }
-        .opacity(min(configuration.fractionCompleted ?? 0 * 9, 1))
         .frame(width: 28, height: 28)
     }
 }
