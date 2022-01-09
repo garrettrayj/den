06478ba29c3196c598c9ce0c95176d0e845d8bb0
Garrett Johnson <garrettrayj@gmail.com>
2022-01-09 04:30:09 -0700

    Use SectionedFetchRequest in history view.
    Former-commit-id: b68a9afbfe72838d7a27b86d131911549fc14040

--- Den/Views/HistoryView.swift
+++ Den/Views/HistoryView.swift
@@ -12,16 +12,51 @@ import SwiftUI
 struct HistoryView: View {
     @EnvironmentObject var linkManager: LinkManager
 
-    @FetchRequest(sortDescriptors: [SortDescriptor(\.visited, order: .reverse)])
-    private var history: FetchedResults<History>
+    @SectionedFetchRequest<String, History>(sectionIdentifier: \.day, sortDescriptors: [])
+    private var historySections: SectionedFetchResults<String, History>
 
     var body: some View {
         VStack(spacing: 0) {
-            if history.count > 0 {
+            if historySections.count > 0 {
                 ScrollView {
                     LazyVStack(alignment: .leading, spacing: 0, pinnedViews: .sectionHeaders) {
-                        ForEach(groupedResults, id: \.self) { resultGroup in
-                            resultsSection(resultGroup: resultGroup)
+                        ForEach(historySections) { section in
+                            Section {
+                                VStack(spacing: 0) {
+                                    ForEach(section) { result in
+                                        if result.title != nil && result.link != nil {
+                                            VStack(alignment: .leading, spacing: 2) {
+                                                Button { linkManager.openLink(url: result.link) } label: {
+                                                    Text(result.title!)
+                                                        .font(.title3)
+                                                        .multilineTextAlignment(.leading)
+                                                        .foregroundColor(Color(UIColor.systemPurple))
+                                                }
+                                                Text(result.link!.absoluteString)
+                                                    .font(.caption)
+                                                    .foregroundColor(Color.secondary)
+                                                    .lineLimit(1)
+                                            }
+                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                            .padding(.horizontal, 12)
+                                            .padding(.vertical, 8)
+
+                                            if section.last != result {
+                                                Divider()
+                                            }
+                                        }
+                                    }
+                                }
+                                .background(Color(UIColor.systemBackground))
+                                .cornerRadius(8)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 8)
+                                        .strokeBorder(Color(UIColor.secondarySystemGroupedBackground))
+                                )
+                                .padding()
+                            } header: {
+                                Text(section.id).font(.subheadline).modifier(PinnedSectionHeaderModifier())
+                            }
                         }
                     }
                 }
@@ -34,60 +69,17 @@ struct HistoryView: View {
         .navigationTitle("History")
     }
 
-    private var groupedResults: [[History]] {
-        let compacted: [History] = history.compactMap { history in
-            history.visited != nil ? history : nil
-        }
-
-        let results = Dictionary(
-            grouping: compacted,
-            by: { DateFormatter.fullNone.string(from: $0.visited!) }
+    init(profile: Profile) {
+        let profilePredicate = NSPredicate(
+            format: "profile.id == %@",
+            profile.id?.uuidString ?? ""
         )
 
-        return results.values.sorted { aHistory, bHistory in
-            return aHistory[0].visited! > bHistory[0].visited!
-        }
-    }
-
-    private func resultsSection(resultGroup: [History]) -> some View {
-        Section(
-            header:
-                Text("\(resultGroup.first!.visited!, formatter: DateFormatter.fullNone)")
-                    .font(.subheadline)
-                    .modifier(PinnedSectionHeaderModifier())
-        ) {
-            VStack(spacing: 0) {
-                ForEach(resultGroup) { result in
-                    if result.title != nil && result.link != nil {
-                        VStack(alignment: .leading, spacing: 2) {
-                            Button { linkManager.openLink(url: result.link) } label: {
-                                Text(result.title!)
-                                    .font(.title3)
-                                    .multilineTextAlignment(.leading)
-                                    .foregroundColor(Color(UIColor.systemPurple))
-                            }
-                            Text(result.link!.absoluteString)
-                                .font(.caption)
-                                .foregroundColor(Color.secondary)
-                                .lineLimit(1)
-                        }
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.horizontal, 12)
-                        .padding(.vertical, 10)
-
-                        if resultGroup.last != result {
-                            Divider()
-                        }
-                    }
-                }
-            }
-            .background(Color(UIColor.systemBackground))
-            .cornerRadius(8)
-            .overlay(
-                RoundedRectangle(cornerRadius: 8)
-                    .strokeBorder(Color(UIColor.secondarySystemGroupedBackground))
-            )
-            .padding()
-        }
+        _historySections = SectionedFetchRequest<String, History>(
+            entity: History.entity(),
+            sectionIdentifier: \.day,
+            sortDescriptors: [NSSortDescriptor(keyPath: \History.visited, ascending: true)],
+            predicate: profilePredicate
+        )
     }
 }
