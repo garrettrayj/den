f37b861439c57b297e3365b96d93eb0a5c7174ab
Garrett Johnson <garrettrayj@gmail.com>
2022-09-25 12:33:39 -0600

    Misc fixes.
    Former-commit-id: 72d73948972976b849a6b3fe83a1caf359040e66

--- Den/Views/WelcomeView.swift
+++ Den/Views/WelcomeView.swift
@@ -12,9 +12,15 @@ struct WelcomeView: View {
     @ObservedObject var profile: Profile
 
     var body: some View {
-        VStack(spacing: 8) {
-            Text("Active profile:").foregroundColor(.secondary)
+        VStack(spacing: 16) {
+            Spacer()
+            ZStack(alignment: .center) {
+                Image(systemName: "hexagon").font(.system(size: 48))
+                Image(systemName: "bolt.fill").font(.system(size: 24)).foregroundColor(.accentColor)
+            }
+
             Text(profile.displayName).font(.largeTitle.weight(.semibold))
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(UIColor.systemGroupedBackground).edgesIgnoringSafeArea(.all))
