fd11841df8f99245106670a9f1b30bd55b4e84bb
Garrett Johnson <garrettrayj@gmail.com>
2023-02-04 03:35:32 -0700

    Add deck layout view mode.
    Former-commit-id: 5f406ad898aeb43a159a5d667aea6a1682a69cc4

--- Den/Interface/Styles/FeedTitleButtonStyle.swift
+++ Den/Interface/Styles/FeedTitleButtonStyle.swift
@@ -23,7 +23,7 @@ struct FeedTitleButtonStyle: ButtonStyle {
                 isEnabled ? .primary : .secondary
             )
             .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.vertical, 8)
+            .frame(height: 36)
             .padding(.horizontal, 12)
             .background(
                 backgroundColor.overlay {
