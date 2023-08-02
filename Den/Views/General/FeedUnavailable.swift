0c8adcdea2bd2402963bb6d085fa7d4f821b58a4
Garrett Johnson <garrettrayj@gmail.com>
2023-08-01 23:59:31 -0500

    Attempt to fix build bug.

--- Den/Views/General/FeedUnavailable.swift
+++ Den/Views/General/FeedUnavailable.swift
@@ -16,38 +16,36 @@ struct FeedUnavailable: View {
     let feedData: FeedData?
 
     var body: some View {
-        VStack(spacing: 4) {
-            if feedData == nil {
-                CardNote(
-                    Text("No Data", comment: "Feed unavailable message."),
-                    caption: {
-                        Text("Refresh to fetch items.", comment: "Feed unavailable message.")
-                    },
-                    icon: { Image(systemName: "bolt.horizontal") }
-                )
-            } else if feedData?.wrappedError != nil {
-                CardNote(
-                    Text("Refresh Failed", comment: "Feed unavailable message."),
-                    caption: {
-                        if feedData?.wrappedError == .request {
-                            Text("Could not fetch data.", comment: "Feed unavailable message.")
-                        } else {
-                            Text("Unable to parse content.", comment: "Feed unavailable message.")
-                        }
-                    },
-                    icon: { Image(systemName: "exclamationmark.triangle") }
-                )
-            } else if feedData!.itemsArray.isEmpty {
-                CardNote(
-                    Text("Feed Empty", comment: "Feed unavailable message."),
-                    icon: { Image(systemName: "circle.slash" ).scaleEffect(x: -1, y: 1) }
-                )
-            } else {
-                CardNote(
-                    Text("Feed Unavailable", comment: "Feed unavailable message."),
-                    icon: { Image(systemName: "questionmark.diamond" ) }
-                )
-            }
+        if feedData == nil {
+            CardNote(
+                Text("No Data", comment: "Feed unavailable message."),
+                caption: {
+                    Text("Refresh to fetch items.", comment: "Feed unavailable message.")
+                },
+                icon: { Image(systemName: "bolt.horizontal") }
+            )
+        } else if feedData?.wrappedError != nil {
+            CardNote(
+                Text("Refresh Failed", comment: "Feed unavailable message."),
+                caption: {
+                    if feedData?.wrappedError == .request {
+                        Text("Could not fetch data.", comment: "Feed unavailable message.")
+                    } else {
+                        Text("Unable to parse content.", comment: "Feed unavailable message.")
+                    }
+                },
+                icon: { Image(systemName: "exclamationmark.triangle") }
+            )
+        } else if feedData!.itemsArray.isEmpty {
+            CardNote(
+                Text("Feed Empty", comment: "Feed unavailable message."),
+                icon: { Image(systemName: "circle.slash" ).scaleEffect(x: -1, y: 1) }
+            )
+        } else {
+            CardNote(
+                Text("Feed Unavailable", comment: "Feed unavailable message."),
+                icon: { Image(systemName: "questionmark.diamond" ) }
+            )
         }
     }
 }
