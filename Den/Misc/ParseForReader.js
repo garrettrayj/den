e7b510fa0b2f7673cd535658f1e5041996124c0b
Garrett Johnson <garrettrayj@gmail.com>
2023-10-19 08:08:53 -0500

    Update parse for reader script.

--- Den/Misc/ParseForReader.js
+++ Den/Misc/ParseForReader.js
@@ -1,15 +1,5 @@
-function parseForReader() {
-    Mercury.parse(document.URL).then(function(result) {
+window.addEventListener("pageshow", function(event) {
+    Mercury.parse().then(function(result) {
         window.webkit.messageHandlers.reader.postMessage(JSON.stringify(result));
     });
-}
-
-window.addEventListener("pageshow", function(event) {
-    if (event.persisted) {
-        parseForReader();
-    }
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-    parseForReader();
 });
