5979effda30393ba28661811c0568515a5234b7c
Garrett Johnson <garrettrayj@gmail.com>
2024-05-05 11:52:34 -0500

    Improve feed item compressed view.

--- Den/Views/Item/FeedItemCompressed.swift
+++ Den/Views/Item/FeedItemCompressed.swift
@@ -16,28 +16,26 @@ struct FeedItemCompressed: View {
 
     var body: some View {
         ItemActionView(item: item, isLastInList: true, isStandalone: true) {
-            VStack(alignment: .leading, spacing: 8) {
-                FeedTitleLabel(feed: feed).font(.callout).imageScale(.small)
-                
-                HStack(alignment: .top) {
-                    VStack(alignment: .leading, spacing: 4) {
-                        PreviewHeadline(title: item.titleText)
-                        
-                        if !feed.hideBylines, let author = item.author {
-                            PreviewAuthor(author: author)
-                        }
-                        if !item.bookmarks.isEmpty {
-                            ItemTags(item: item)
-                        }
-                        if let date = item.published {
-                            PreviewDateline(date: date)
-                        }                    }
+            HStack(alignment: .top) {
+                VStack(alignment: .leading, spacing: 4) {
+                    FeedTitleLabel(feed: feed).font(.callout).imageScale(.small)
                     
-                    Spacer(minLength: 0)
-
-                    if !feed.hideImages, let url = item.image {
-                        SmallThumbnail(url: url, isRead: item.read).padding(.leading, 12)
+                    PreviewHeadline(title: item.titleText)
+                    
+                    if !feed.hideBylines, let author = item.author {
+                        PreviewAuthor(author: author)
                     }
+                    if !item.bookmarks.isEmpty {
+                        ItemTags(item: item)
+                    }
+                    if let date = item.published {
+                        PreviewDateline(date: date)
+                    }                    }
+                
+                Spacer(minLength: 0)
+
+                if !feed.hideImages, let url = item.image {
+                    SmallThumbnail(url: url, isRead: item.read).padding(.leading, 12)
                 }
             }
             .padding(12)
