11521358642919d5369dddf42ba41a0153704411
Garrett Johnson <garrettrayj@gmail.com>
2024-06-19 03:29:39 -0500

    Improve tags menu.

--- Den/Views/Item/TagsMenu.swift
+++ Den/Views/Item/TagsMenu.swift
@@ -23,60 +23,48 @@ struct TagsMenu: View {
 
     var body: some View {
         Menu {
-            if !tags.isEmpty {
-                ForEach(tags) { tag in
-                    if item.bookmarkTags.contains(tag) {
-                        Button {
-                            for bookmark in item.bookmarks where bookmark.tag == tag {
-                                viewContext.delete(bookmark)
-                            }
-                            do {
-                                try viewContext.save()
-                                item.objectWillChange.send()
-                            } catch {
-                                CrashUtility.handleCriticalError(error as NSError)
-                            }
-                        } label: {
-                            Label { tag.displayName } icon: {
-                                Image(systemName: "tag.fill")
-                            }
-                        }
-                        .labelStyle(.titleAndIcon)
-                        .accessibilityIdentifier("RemoveBookmark")
-                    } else {
-                        Button {
-                            _ = Bookmark.create(in: viewContext, item: item, tag: tag)
-                            do {
-                                try viewContext.save()
-                                item.objectWillChange.send()
-                            } catch {
-                                CrashUtility.handleCriticalError(error as NSError)
-                            }
-                        } label: {
-                            Label { tag.displayName } icon: {
-                                Image(systemName: "tag")
-                            }
-                        }
-                        .labelStyle(.titleAndIcon)
-                        .accessibilityIdentifier("AddBookmark")
-                    }
-                }
-            } else {
+            if tags.isEmpty {
                 Text("No Tags", comment: "Menu options unavailable message.")
+            } else {
+                ForEach(tags) { tag in
+                    Button {
+                        toggleTag(tag: tag)
+                    } label: {
+                        Label { tag.displayName } icon: {
+                            Image(systemName: "tag")
+                                .symbolVariant(item.bookmarks.count > 0 ? .fill : .none)
+                        }
+                    }
+                    .labelStyle(.titleAndIcon)
+                    .accessibilityIdentifier("ToggleTag")
+                }
             }
         } label: {
             Label {
                 Text("Tags", comment: "Menu label.")
             } icon: {
-                if item.bookmarks.count > 0 {
-                    Image(systemName: "tag.fill")
-                } else {
-                    Image(systemName: "tag")
-                }
+                Image(systemName: "tag").symbolVariant(item.bookmarks.count > 0 ? .fill : .none)
             }
         }
         .menuIndicator(.hidden)
         .help(Text("Select Tags", comment: "Menu help text."))
         .accessibilityIdentifier("TagsMenu")
+    }
+    
+    private func toggleTag(tag: Tag) {
+        if item.bookmarkTags.contains(tag) {
+            for bookmark in item.bookmarks where bookmark.tag == tag {
+                viewContext.delete(bookmark)
+            }
+        } else {
+            _ = Bookmark.create(in: viewContext, item: item, tag: tag)
+        }
+        
+        do {
+            try viewContext.save()
+            item.objectWillChange.send()
+        } catch {
+            CrashUtility.handleCriticalError(error as NSError)
+        }
     }
 }
