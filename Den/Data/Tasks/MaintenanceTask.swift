05739d80a0e4a46cb77d1646be7ad52a686e5907
Garrett Johnson <garrettrayj@gmail.com>
2024-06-25 03:30:24 -0500

    Update maintenance task for SwiftData.

--- Den/Data/Tasks/MaintenanceTask.swift
+++ Den/Data/Tasks/MaintenanceTask.swift
@@ -31,24 +31,32 @@ struct MaintenanceTask {
     }
     
     private func trimHistory(context: ModelContext) {
-        /*
-        DataController.truncate(
-            History.self,
-            context: context,
-            sortDescriptors: [NSSortDescriptor(keyPath: \History.visited, ascending: false)],
-            offset: 100000
+        var fetchDescriptor = FetchDescriptor<History>(
+            sortBy: [SortDescriptor(\.visited, order: .reverse)]
         )
-         */
+        fetchDescriptor.fetchOffset = 100000
+        
+        guard let history = try? context.fetch(fetchDescriptor) else {
+            return
+        }
+        
+        for record in history {
+            context.delete(record)
+        }
     }
     
     private func trimSearches(context: ModelContext) {
-        /*
-        DataController.truncate(
-            Search.self,
-            context: context,
-            sortDescriptors: [NSSortDescriptor(keyPath: \Search.submitted, ascending: false)],
-            offset: 20
+        var fetchDescriptor = FetchDescriptor<Search>(
+            sortBy: [SortDescriptor(\.submitted, order: .reverse)]
         )
-         */
+        fetchDescriptor.fetchOffset = 20
+        
+        guard let searches = try? context.fetch(fetchDescriptor) else {
+            return
+        }
+        
+        for search in searches {
+            context.delete(search)
+        }
     }
 }
