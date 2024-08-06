7ad483570358e5c714289c380680dd2416839f87
Garrett Johnson <garrettrayj@gmail.com>
2024-08-06 01:51:07 -0500

    Add screenshots test target.

---
+++ Screenshots/ScreenshotsLaunchTests.swift
@@ -0,0 +1,36 @@
+//
+//  ScreenshotsLaunchTests.swift
+//  Screenshots
+//
+//  Created by Garrett Johnson on 8/6/24.
+//  Copyright © 2024 
+//
+//  SPDX-License-Identifier: MIT
+//
+
+import XCTest
+
+final class ScreenshotsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
